08e19029e393d0ab41cdcd89f5280d9cf61d6dff
Michael Voell <sdcored@gmail.com>
2016-01-13 09:14:17 -0600

    Class is ending. Rip.

--- AluminumRecyclingCampaign/AluminumRecyclingCampaign/frmAluminumRecycling.vb
+++ AluminumRecyclingCampaign/AluminumRecyclingCampaign/frmAluminumRecycling.vb
@@ -5,4 +5,8 @@
         txtAmount.Text = ""
         lblOutput.Text = "0,000 cans need to be collected to reach your goal"
     End Sub
+
+    Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
+
+    End Sub
 End Class
