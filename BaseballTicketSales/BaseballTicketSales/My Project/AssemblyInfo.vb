fe497eecec39ef76ecb5ab60ae338d4bff62abea
Michael Voell <sdcored@gmail.com>
2016-01-19 07:52:40 -0600

    Initial Project Setup
    Setting up BaseballTicketSales project.

---
+++ BaseballTicketSales/BaseballTicketSales/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("BaseballTicketSales")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Mundelein High School")> 
+<Assembly: AssemblyProduct("BaseballTicketSales")> 
+<Assembly: AssemblyCopyright("Copyright © Mundelein High School 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bcffe768-f50a-42e7-a972-fae3d9114d3f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ BaseballTicketSales/BaseballTicketSales/frmBaseballTicketSales.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmBaseballTicketSales
+
+End Class
