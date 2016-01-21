39261802c9d53bee7f3f57711b7dcce3b7199380
Michael Voell <sdcored@gmail.com>
2016-01-21 10:25:35 -0600

    More initial setup + design

--- CampgroundRates/CampgroundRates/frmCampgroundRates.Designer.vb
+++ CampgroundRates/CampgroundRates/frmCampgroundRates.Designer.vb
@@ -22,17 +22,61 @@ Partial Class frmCampgroundRates
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmCampgroundRates))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.cboStayType = New System.Windows.Forms.ComboBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(459, 221)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.Color.Transparent
+        Me.lblTitle.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(109, 13)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(267, 30)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Misty River Campgrounds"
+        '
+        'cboStayType
+        '
+        Me.cboStayType.FormattingEnabled = True
+        Me.cboStayType.Location = New System.Drawing.Point(13, 241)
+        Me.cboStayType.Name = "cboStayType"
+        Me.cboStayType.Size = New System.Drawing.Size(459, 21)
+        Me.cboStayType.TabIndex = 2
+        Me.cboStayType.Text = "Select an Item"
         '
         'frmCampgroundRates
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(484, 462)
+        Me.Controls.Add(Me.cboStayType)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "frmCampgroundRates"
         Me.Text = "Campground Rates"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents lblTitle As System.Windows.Forms.Label
+    Friend WithEvents cboStayType As System.Windows.Forms.ComboBox
 
 End Class
