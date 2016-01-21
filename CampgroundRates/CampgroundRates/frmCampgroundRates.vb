d31afbf71bf60d5b871fccab0b558825ca84b8b3
Michael Voell <sdcored@gmail.com>
2016-01-21 10:13:09 -0600

    Initial project setup for Campground Rates

---
+++ CampgroundRates/CampgroundRates/frmCampgroundRates.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmCampgroundRates
+
+    Private Sub frmCampgroundRates_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Threading.Thread.Sleep(5000)
+    End Sub
+End Class
