85fb7548124c48b7d91d37f5674d30709f54b751
Michael Voell <sdcored@gmail.com>
2016-01-22 08:18:56 -0600

    Stuff. Things... It's Friday, I'm trying

--- CampgroundRates/CampgroundRates/frmCampgroundRates.vb
+++ CampgroundRates/CampgroundRates/frmCampgroundRates.vb
@@ -4,14 +4,11 @@
 ' Purpose: To calculate the cost of a stay at campgrounds under different conditions.
 
 ' --TODO--
-' Combobox -> Requests type of tent -> Tent: $20/night, RV: $35/night, Cabin: $55/night
-' Textbox -> Number of nights staying
-' Combobox -> AAA (10% discount) or Military (12% discount)
-' Subprocedure displays: Subtotal Billing Amount for your Stay, Taxes for your Stay, and Final Total
 ' Subprocedure -> Type of Tent Cost * Amount of Nights Staying = Subtotal. Do not displaying amount (keep for next equation)
 ' Pass the subtotal + discount into a Function. Determine = Subtotal / Discount. Subtotal = Determine. Pass to calling method
 ' Another function should compute 7.5% tax. Total = Subtotal / 7.5%. TheTotal = Total + Subtotal. Pass back to calling method
 ' Pass all values to a sub procedure to displaying results.
+' Subprocedure displays: Subtotal Billing Amount for your Stay, Taxes for your Stay, and Final Total
 ' Input values need a Try-Catch block
 
 Option Strict On
@@ -20,5 +17,34 @@ Public Class frmCampgroundRates
 
     Private Sub frmCampgroundRates_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         Threading.Thread.Sleep(5000)
+    End Sub
+
+    Private Sub btnCalculateCost_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculateCost.Click
+        ' Variables
+        Dim decTentCost As Decimal
+        Dim decNightsStaying As Decimal
+        Dim decDiscount As Decimal
+
+        decNightsStaying = CDec(txtNumberOfNights.Text)
+
+        decTentCost = cboStayType.SelectedIndex
+        Select Case decTentCost
+            Case 0
+                CalculateSubTotal(20, decNightsStaying)
+            Case 1
+                CalculateSubTotal(35, decNightsStaying)
+            Case 2
+                CalculateSubTotal(55, decNightsStaying)
+        End Select
+
+    End Sub
+
+    Private Sub CalculateSubTotal(ByVal decTheTentCost As Decimal, ByVal decTheNightsStaying As Decimal)
+        ' Variables
+        Dim decSubTotal As Decimal
+
+        ' Calculations
+        decSubTotal = decTheTentCost * decTheNightsStaying
+
     End Sub
 End Class
