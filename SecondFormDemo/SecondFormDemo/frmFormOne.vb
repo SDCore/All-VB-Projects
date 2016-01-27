6db1125156d4e458a59b87df875b2cab6fc6b4ad
Michael Voell <sdcored@gmail.com>
2016-01-27 08:09:28 -0600

    Forms. Stuff. YAY

--- SecondFormDemo/SecondFormDemo/frmFormOne.vb
+++ SecondFormDemo/SecondFormDemo/frmFormOne.vb
@@ -2,5 +2,13 @@
 
     Private Sub btnShowNewForm_Click(sender As System.Object, e As System.EventArgs) Handles btnShowNewForm.Click
         Dim frmSecond As New frmFormTwo ' Declaring an reference variable and creating an frmFormTwo instance
+
+        ' Hide() 'Hides current form
+
+        ' Line below creats a modal form (retains the focus while open; cannot switch between form objects until the first form is closed)
+        ' frmSecond.ShowDialog() ' ShowDialog function is being called on frmSecond reference
+
+        frmSecond.Show() ' Creates a modeless form (allows both forms to be focused)
+
     End Sub
 End Class
