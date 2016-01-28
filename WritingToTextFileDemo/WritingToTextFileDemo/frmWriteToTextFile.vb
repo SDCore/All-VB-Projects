ad8d3a58d84726163fe5e8899b3365729b6acf71
Michael Voell <sdcored@gmail.com>
2016-01-28 10:03:25 -0600

    Help fa

--- WritingToTextFileDemo/WritingToTextFileDemo/frmWriteToTextFile.vb
+++ WritingToTextFileDemo/WritingToTextFileDemo/frmWriteToTextFile.vb
@@ -12,8 +12,13 @@
             strNames(intCount) = InputBox("Enter Name " & (intCount + 1), "Enter Information", "")
             intID(intCount) = InputBox("Enter ID # for Student " & (intCount + 1), "Enter Information", "")
 
-            objWriter.WriteLine(strNames(intCount)) ' Writes the array element of the Name to the text file
-            objWriter.WriteLine(intID(intCount)) ' Writes the array element of the ID to the text file
+            If IO.File.Exists("E:\Advanced VB\WritingToTextFileDemo\WritingToTextFileDemo\roster.txt") Then
+                objWriter.WriteLine(strNames(intCount)) ' Writes the array element of the Name to the text file
+                objWriter.WriteLine(intID(intCount)) ' Writes the array element of the ID to the text file
+            Else
+                MsgBox("File not available. Please re-run program when it is available.")
+            End If
+
         Next
 
         objWriter.Close()
