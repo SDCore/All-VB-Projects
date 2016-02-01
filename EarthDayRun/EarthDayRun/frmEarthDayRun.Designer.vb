7ce7e1ccbd36e0863da3622066b0830c5a614461
Michael Voell <sdcored@gmail.com>
2016-02-01 08:04:28 -0600

    Earth Day Run Setup

---
+++ EarthDayRun/EarthDayRun/frmEarthDayRun.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEarthDayRun
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnComputeFunds = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnComputeFunds
+        '
+        Me.btnComputeFunds.Location = New System.Drawing.Point(13, 523)
+        Me.btnComputeFunds.Name = "btnComputeFunds"
+        Me.btnComputeFunds.Size = New System.Drawing.Size(813, 23)
+        Me.btnComputeFunds.TabIndex = 0
+        Me.btnComputeFunds.Text = "Calculate Funds Raised"
+        Me.btnComputeFunds.UseVisualStyleBackColor = True
+        '
+        'frmEarthDayRun
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(838, 558)
+        Me.Controls.Add(Me.btnComputeFunds)
+        Me.Name = "frmEarthDayRun"
+        Me.Text = "Earth Day Run"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnComputeFunds As System.Windows.Forms.Button
+
+End Class
---
+++ EarthDayRun/EarthDayRun/frmEarthDayRun.vb
@@ -0,0 +1,11 @@
+﻿' Michael Voell
+' Earth Day Run
+' 2/1/2016
+' Purpose: To determine the total contributions recieved by the fund raising runners at the Earth Day 5K Run.
+
+Public Class frmEarthDayRun
+
+    Private Sub btnComputeFunds_Click(sender As System.Object, e As System.EventArgs) Handles btnComputeFunds.Click
+
+    End Sub
+End Class
