7af5b3adf05817dc92267691a591a163405f46b1
Michael Voell <sdcored@gmail.com>
2016-02-09 08:20:16 -0600

    Making breakthou-... I mean bacon. Makin' Bacon.

--- ProjectedPopulationGrowth/ProjectedPopulationGrowth/frmProjectedPopulation.vb
+++ ProjectedPopulationGrowth/ProjectedPopulationGrowth/frmProjectedPopulation.vb
@@ -9,16 +9,37 @@
 ' File menu displays the "Display Present Population", "Clear", and "Exit" menu items.
 '  When user selects "Display Present Population", a second form opens and siaplys the current 10 largest cities and their populations
 
+Option Strict On
+
 Public Class frmProjectedPopulation
+
+    Public Shared _intArraySize As Integer = 20
+    Private _strCityNames(_intArraySize) As String
 
     Private Sub frmProjectedPopulation_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         ' Variables
         Dim objReader As IO.StreamReader
         Dim filePath As String = "E:\Advanced VB\ProjectedPopulationGrowth\ProjectedPopulationGrowth\cities.txt"
+        Dim intCount As Integer = 0
+        Dim intFill As Integer
 
         ' Checking is file exists... and everything else
         If IO.File.Exists(filePath) Then
+            objReader = IO.File.OpenText(filePath)
+            Do While objReader.Peek <> -1
+                _strCityNames(intCount) = objReader.ReadLine()
+                intCount += 1
+            Loop
+            objReader.Close()
 
+            ' Populate ComboBox with city names
+            For intFill = 0 To (_strCityNames.Length - 1)
+                If intFill Mod 2 = 0 Then
+                    cboSelectCity.Items.Add(_strCityNames(intFill))
+                End If
+            Next
+        Else
+            MsgBox("Could not find the file. Please try again.", , "ERROR")
         End If
     End Sub
 
