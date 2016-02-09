4ba438df78906d69665bb185de08e78a611d77b3
Michael Voell <sdcored@gmail.com>
2016-02-09 08:35:32 -0600

    Removal
    Had to remove the comment from --TODO-- because it's complete

--- ProjectedPopulationGrowth/ProjectedPopulationGrowth/frmProjectedPopulation.vb
+++ ProjectedPopulationGrowth/ProjectedPopulationGrowth/frmProjectedPopulation.vb
@@ -4,7 +4,6 @@
 ' Purpose: To determine the population growth of cities over the next 5 years
 
 ' --TODO--
-' ComboBox (cboSelectCity) is populated from the text file, contains all 10 cities from text file
 ' User selects city from cboSelectCity, ListBox (lstPopulationGrowth) displays next 5 years of population growth based on 3% population growth
 ' File menu displays the "Display Present Population", "Clear", and "Exit" menu items.
 '  When user selects "Display Present Population", a second form opens and siaplys the current 10 largest cities and their populations
