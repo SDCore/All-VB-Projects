d22c35a53f940ab548b9e33a7fa0685f1615cf1a
Michael Voell <sdcored@gmail.com>
2016-02-10 09:11:48 -0600

    Class is ending. Rip.

--- DJPlayList/DJPlayList/My Project/Application.Designer.vb
+++ DJPlayList/DJPlayList/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.DJPlayList.Form1
+            Me.MainForm = Global.DJPlayList.frmDJPlayList
         End Sub
     End Class
 End Namespace
