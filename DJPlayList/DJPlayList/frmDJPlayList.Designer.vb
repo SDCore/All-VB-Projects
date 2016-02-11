83179fa01a7c0bc22ec419efc6633d3ca6e631dd
Michael Voell <sdcored@gmail.com>
2016-02-11 09:36:29 -0600

    Design

--- DJPlayList/DJPlayList/frmDJPlayList.Designer.vb
+++ DJPlayList/DJPlayList/frmDJPlayList.Designer.vb
@@ -23,18 +23,91 @@ Partial Class frmDJPlayList
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmDJPlayList))
+        Me.btnShowList = New System.Windows.Forms.Button()
+        Me.lblSelectType = New System.Windows.Forms.Label()
+        Me.cboMusicGenre = New System.Windows.Forms.ComboBox()
+        Me.lblSelectedSongs = New System.Windows.Forms.Label()
+        Me.lstSelectedSongs = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
+        '
+        'btnShowList
+        '
+        Me.btnShowList.BackColor = System.Drawing.Color.Black
+        Me.btnShowList.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnShowList.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnShowList.ForeColor = System.Drawing.Color.Red
+        Me.btnShowList.Location = New System.Drawing.Point(12, 371)
+        Me.btnShowList.Name = "btnShowList"
+        Me.btnShowList.Size = New System.Drawing.Size(460, 80)
+        Me.btnShowList.TabIndex = 0
+        Me.btnShowList.Text = "Show Music Playlist"
+        Me.btnShowList.UseVisualStyleBackColor = False
+        '
+        'lblSelectType
+        '
+        Me.lblSelectType.AutoSize = True
+        Me.lblSelectType.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSelectType.ForeColor = System.Drawing.Color.Red
+        Me.lblSelectType.Location = New System.Drawing.Point(12, 9)
+        Me.lblSelectType.Name = "lblSelectType"
+        Me.lblSelectType.Size = New System.Drawing.Size(150, 21)
+        Me.lblSelectType.TabIndex = 1
+        Me.lblSelectType.Text = "Select Music Genre"
+        '
+        'cboMusicGenre
+        '
+        Me.cboMusicGenre.FormattingEnabled = True
+        Me.cboMusicGenre.Location = New System.Drawing.Point(12, 34)
+        Me.cboMusicGenre.Name = "cboMusicGenre"
+        Me.cboMusicGenre.Size = New System.Drawing.Size(460, 21)
+        Me.cboMusicGenre.TabIndex = 2
+        '
+        'lblSelectedSongs
+        '
+        Me.lblSelectedSongs.AutoSize = True
+        Me.lblSelectedSongs.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSelectedSongs.ForeColor = System.Drawing.Color.Red
+        Me.lblSelectedSongs.Location = New System.Drawing.Point(12, 77)
+        Me.lblSelectedSongs.Name = "lblSelectedSongs"
+        Me.lblSelectedSongs.Size = New System.Drawing.Size(126, 21)
+        Me.lblSelectedSongs.TabIndex = 3
+        Me.lblSelectedSongs.Text = "Songs in Playlist"
+        '
+        'lstSelectedSongs
+        '
+        Me.lstSelectedSongs.BackColor = System.Drawing.Color.Black
+        Me.lstSelectedSongs.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstSelectedSongs.ForeColor = System.Drawing.Color.Red
+        Me.lstSelectedSongs.FormattingEnabled = True
+        Me.lstSelectedSongs.ItemHeight = 17
+        Me.lstSelectedSongs.Location = New System.Drawing.Point(12, 101)
+        Me.lstSelectedSongs.Name = "lstSelectedSongs"
+        Me.lstSelectedSongs.Size = New System.Drawing.Size(460, 259)
+        Me.lstSelectedSongs.TabIndex = 4
         '
         'frmDJPlayList
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
         Me.ClientSize = New System.Drawing.Size(484, 462)
+        Me.Controls.Add(Me.lstSelectedSongs)
+        Me.Controls.Add(Me.lblSelectedSongs)
+        Me.Controls.Add(Me.cboMusicGenre)
+        Me.Controls.Add(Me.lblSelectType)
+        Me.Controls.Add(Me.btnShowList)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frmDJPlayList"
         Me.Text = "DJ Hoster Playlist"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents btnShowList As System.Windows.Forms.Button
+    Friend WithEvents lblSelectType As System.Windows.Forms.Label
+    Friend WithEvents cboMusicGenre As System.Windows.Forms.ComboBox
+    Friend WithEvents lblSelectedSongs As System.Windows.Forms.Label
+    Friend WithEvents lstSelectedSongs As System.Windows.Forms.ListBox
 
 End Class
