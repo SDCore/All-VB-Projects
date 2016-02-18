fa97a9c98e485a3d38ee6b26cd69dd0ff9647d0c
Michael Voell <sdcored@gmail.com>
2016-02-18 10:08:50 -0600

    Design Stuff

--- ApprovedTravelRequests/ApprovedTravelRequests/frmApprovedTravel.Designer.vb
+++ ApprovedTravelRequests/ApprovedTravelRequests/frmApprovedTravel.Designer.vb
@@ -22,9 +22,35 @@ Partial Class frmApprovedTravel
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmApprovedTravel))
+        Me.lblFormTitle = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblFormTitle
+        '
+        Me.lblFormTitle.AutoSize = True
+        Me.lblFormTitle.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFormTitle.ForeColor = System.Drawing.Color.LightSeaGreen
+        Me.lblFormTitle.Location = New System.Drawing.Point(12, 9)
+        Me.lblFormTitle.Name = "lblFormTitle"
+        Me.lblFormTitle.Size = New System.Drawing.Size(474, 37)
+        Me.lblFormTitle.TabIndex = 0
+        Me.lblFormTitle.Text = "Intuition Approved Travel Requests"
+        '
+        'frmApprovedTravel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(500, 533)
+        Me.Controls.Add(Me.lblFormTitle)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmApprovedTravel"
+        Me.Text = "Intuition Approved Travel Requests"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents lblFormTitle As System.Windows.Forms.Label
 
 End Class
