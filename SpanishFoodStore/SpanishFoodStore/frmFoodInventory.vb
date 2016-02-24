f3097252c618746b74c49d521852176e80150da8
Michael Voell <sdcored@gmail.com>
2016-02-24 07:57:32 -0600

    Spanish Foods TODO

--- SpanishFoodStore/SpanishFoodStore/frmFoodInventory.vb
+++ SpanishFoodStore/SpanishFoodStore/frmFoodInventory.vb
@@ -3,6 +3,11 @@
 ' 2/23/2016
 ' Purpose: Show the inventory of a Spanish Food Store and calculate the total value of the intentory.
 
+' --TODO--
+' Compute Total Button to compute total value of the store's inventory
+' Total Value Equation: (Cost Per Item * The Quantity of the Item) + Accumulator
+' Try/Catch for when database is not available
+
 Option Strict On
 
 Public Class frmFoodInventory
