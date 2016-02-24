720e13d2f1febb47137f5671d33cd5d7aaf36b33
Michael Voell <sdcored@gmail.com>
2016-02-24 08:13:44 -0600

    [Finished] Spanish Food Store

--- SpanishFoodStore/SpanishFoodStore/frmFoodInventory.vb
+++ SpanishFoodStore/SpanishFoodStore/frmFoodInventory.vb
@@ -3,11 +3,6 @@
 ' 2/23/2016
 ' Purpose: Show the inventory of a Spanish Food Store and calculate the total value of the intentory.
 
-' --TODO--
-' Compute Total Button to compute total value of the store's inventory
-' Total Value Equation: (Cost Per Item * The Quantity of the Item) + Accumulator
-' Try/Catch for when database is not available
-
 Option Strict On
 
 Public Class frmFoodInventory
@@ -21,7 +16,16 @@ Public Class frmFoodInventory
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'SpanishfoodDataSet.StoreInventory' table. You can move, or remove it, as needed.
-        Me.StoreInventoryTableAdapter.Fill(Me.SpanishfoodDataSet.StoreInventory)
+
+        Try
+            Me.StoreInventoryTableAdapter.Fill(Me.SpanishfoodDataSet.StoreInventory)
+        Catch ex As Exception
+            MsgBox("Could not connect to database. Please try again.", , "Database Connection Error")
+        End Try
+
+    End Sub
+
+    Private Sub btnTotalInventory_Click(sender As System.Object, e As System.EventArgs) Handles btnTotalInventory.Click
 
         ' Select / Connecting to database
         Dim strSQL As String = "SELECT * FROM StoreInventory"
@@ -30,6 +34,17 @@ Public Class frmFoodInventory
         Dim datSpanishFoodFill As New DataTable
         odaSpanishFood.Fill(datSpanishFoodFill)
         odaSpanishFood.Dispose()
+
+        ' Variables
+        Dim intCount As Integer = 0
+        Dim decTotal As Decimal
+
+        For intCount = 0 To datSpanishFoodFill.Rows.Count - 1
+            decTotal = decTotal + (Convert.ToDecimal(datSpanishFoodFill.Rows(intCount)("Cost per Item")) * Convert.ToDecimal(datSpanishFoodFill.Rows(intCount)("Quantity")))
+            intCount += 1
+        Next
+
+        lblTotalValue.Text = "Total Value: " & decTotal.ToString("C")
 
     End Sub
 End Class
